a64e16e5fb993f7ba2385a275d9c43cedcee0eca
nykka <veronicaines.garcia@gmail.com>
2022-11-11 15:51:44 -0300

    actualizo los schemas para members

--- studio/schemas/schema.ts
+++ studio/schemas/schema.ts
@@ -5,8 +5,9 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import member from "./member";
 import link from "./link";
+import member from "./member";
+import memberlist from "./memberlist";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -18,5 +19,6 @@ export default createSchema({
     /* Your types here! */
     member,
     link,
+    memberlist,
   ]),
 });
