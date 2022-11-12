deee4b66b33ca3db772e6e27f835c0597fd96b71
LPRegen <manuel.escribano.051@gmail.com>
2022-11-12 14:50:29 -0300

    refactor: include new schemas

--- studio/schemas/schema.ts
+++ studio/schemas/schema.ts
@@ -5,8 +5,13 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import member from "./member";
 import link from "./link";
+import member from "./member";
+import lights from "./Toys/lights";
+import movement from "./Toys/movement";
+import sound from "./Toys/sound";
+import switchInput from "./Toys/switchInput";
+import toy from "./Toys/toy";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -18,5 +23,10 @@ export default createSchema({
     /* Your types here! */
     member,
     link,
+    movement,
+    switchInput,
+    lights,
+    sound,
+    toy,
   ]),
 });
