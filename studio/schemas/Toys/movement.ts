20b0da94db7189fda25794a6db127d49ae9a86ea
LPRegen <manuel.escribano.051@gmail.com>
2022-11-12 09:20:58 -0300

    refactor: add toyName field

--- studio/schemas/Toys/movement.ts
+++ studio/schemas/Toys/movement.ts
@@ -1,49 +1,65 @@
 export default {
   name: "movement",
-  title: "Movimiento",
+  title: "Movimiento del juguete",
   type: "document",
   fields: [
+    {
+      name: "toyName",
+      title: "Nombre del juguete.",
+      type: "string",
+      description: "Indicar el nombre del juguete.",
+      validation: (Rule) => Rule.required(),
+    },
     {
       name: "noMvt",
       title: "Sin movimiento",
       type: "boolean",
       description: "El juguete no tiene movimiento.",
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "forwardMvt",
       title: "Movimiento hacia delante",
       type: "boolean",
       description: "El juguete solo se mueve hacia delante.",
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "backwardsMvt",
       title: "Movimiento hacia atrás",
       type: "boolean",
       description: "El juguete solo se mueve hacia atrás.",
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "leftMvt",
       title: "Movimiento hacia la izquierda",
       type: "boolean",
       description: "El juguete solo se mueve hacia la izquierda.",
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "rightMvt",
       title: "Movimiento hacia la derecha",
       type: "boolean",
       description: "El juguete solo se mueve hacia la derecha.",
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "randomMvt",
       title: "Movimiento aleatorio",
       type: "boolean",
       description: "El juguete tiene movimientos aleatorios.",
+      validation: (Rule) => Rule.required(),
     },
   ],
   preview: {
-    prepare() {
+    select: {
+      title: "toyName",
+    },
+    prepare({ title }) {
       return {
-        title: `Movimiento del juguete.`,
+        title: `Movimiento del juguete "${title}"`,
         subtitle: `Movimientos posibles por el juguete.`,
       };
     },
