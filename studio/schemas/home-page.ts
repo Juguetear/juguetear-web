dee52070b7a553e7aec590ec236d1060b5be6a9d
TonyMckes <tonymckes@gmail.com>
2022-12-03 17:07:33 -0400

    feat: add base text fields

---
+++ studio/schemas/home-page.ts
@@ -0,0 +1,11 @@
+// TODO: Extract to a new schema?, it seen's that it can be reused.
+const textFields = [
+  {
+    name: "title",
+    type: "string",
+  },
+  {
+    name: "description",
+    type: "string",
+  },
+];
