80db11d6e30a5aadfbdc9af1c003720799f06e04
LPRegen <manuel.escribano.051@gmail.com>
2022-12-18 02:44:00 -0300

    refactor: remove types for config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
-/** @type {import('tailwindcss').Config} \*/
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
