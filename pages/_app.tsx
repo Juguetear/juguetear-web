206aa708009fc4e4e551bb4bbb2994463db02f2b
LPRegen <manuel.escribano.051@gmail.com>
2022-12-19 09:01:00 -0300

    feat: specify utilized font's weight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,12 @@ import "../styles/globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
+  weight: "400",
 });
 
 const outfit = Outfit({
   variable: "--font-outfit",
+  weight: "800",
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
