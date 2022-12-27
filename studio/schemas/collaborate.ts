c8481636f426acc8e5736626b0187238f13d8fcd
LPRegen <manuel.escribano.051@gmail.com>
2022-12-27 20:26:20 -0300

    feat: create collaborate page

---
+++ studio/schemas/collaborate.ts
@@ -0,0 +1,43 @@
+import block from "./block";
+
+const contentBlock = [
+  {
+    name: "sectionTitle",
+    title: "Título de la sección",
+    type: "string",
+  },
+  block,
+];
+
+const sectionList = {
+  name: "sectionList",
+  title: "Lista de secciones sobre cómo colaborar con el proyecto",
+  description:
+    "Este listado se mostrará en la página web. Para ordenar el listado, haga click en el primer ícono de la izquierda y arrastre la sección a la posición deseada.",
+  type: "array",
+  of: [
+    {
+      name: "section",
+      type: "object",
+      fields: [...contentBlock],
+    },
+  ],
+};
+
+export default {
+  name: "collaborate",
+  title: "Colaborar con el proyecto",
+  type: "document",
+  fields: [
+    {
+      name: "pageTitle",
+      type: "string",
+      initialValue: "Colaborar con el proyecto",
+      hidden: true,
+    },
+    sectionList,
+  ],
+  preview: {
+    select: { title: "pageTitle" },
+  },
+};
