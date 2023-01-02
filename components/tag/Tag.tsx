06780abf961efd1fbb5f791636f104e727804e41
Damian Palavecino <damianpalavecino25@gmail.com>
2023-01-02 03:43:23 -0300

    feat: add onClick, ariaLabel and className to Tag

--- components/tag/Tag.tsx
+++ components/tag/Tag.tsx
@@ -1,17 +1,23 @@
-import Cross from "components/cross/Cross";
-type Props = {
-    label: string,
-    onClick: (event: React.MouseEvent<HTMLLIElement>) => void,
-}
+import Cross from "../cross/Cross";
 
+type Props = {
+  label: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
 
 const Tag = ({ label, onClick }: Props) => {
   return (
-    <li onClick={onClick} className="bg-gray-light text-blue hover:text-orange fill-blue hover:fill-orange rounded-t-[14px] rounded-b-[14px] border-2 border-white focus-within:shadow-2md">
-        <button type="button" aria-label={`Quitar ${label} de la lista`} className="border-r-[0.5px] h-7 border-gray pl-2 pr-1 focus:outline-none"><Cross /></button>
-        <p className="inline ml-1 mr-6 font-semibold text-sm">{label}</p>
+    <li className="bg-gray-light max-w-fit text-blue hover:text-orange fill-blue hover:fill-orange rounded-t-[14px] rounded-b-[14px] border-2 border-white focus-within:shadow-2md">
+      <Cross
+        onClick={onClick}
+        className="border-r-[0.5px] h-7 border-gray pl-2 pr-1 focus:outline-none"
+        ariaLabel={`Quitar ${label} de la lista`}
+      />
+      <p className="inline ml-1 mr-6 font-semibold font-outfit text-sm">
+        {label}
+      </p>
     </li>
-  )
-}
+  );
+};
 
 export default Tag;
