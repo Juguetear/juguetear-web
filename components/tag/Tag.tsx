e82a6f11c61cd3d3e9129dad243c5c286ad691d3
Damian Palavecino <damianpalavecino25@gmail.com>
2023-01-02 16:02:58 -0300

    fix: remove onClick and ariaLabel props from Cross

--- components/tag/Tag.tsx
+++ components/tag/Tag.tsx
@@ -8,14 +8,15 @@ type Props = {
 const Tag = ({ label, onClick }: Props) => {
   return (
     <li className="bg-gray-light max-w-fit text-blue hover:text-orange fill-blue hover:fill-orange rounded-t-[14px] rounded-b-[14px] border-2 border-white focus-within:shadow-2md">
-      <Cross
+      <button
+        type="button"
         onClick={onClick}
+        aria-label={`Quitar ${label} de la lista`}
         className="border-r-[0.5px] h-7 border-gray pl-2 pr-1 focus:outline-none"
-        ariaLabel={`Quitar ${label} de la lista`}
-      />
-      <p className="inline ml-1 mr-6 font-semibold font-outfit text-sm">
-        {label}
-      </p>
+      >
+        <Cross className="" />
+      </button>
+      <span className="ml-1 mr-6 font-outfit text-sm">{label}</span>
     </li>
   );
 };
