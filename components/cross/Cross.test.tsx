1ea680fad6a12841df1faf545473d3a4e626161a
Damian Palavecino <damianpalavecino25@gmail.com>
2023-01-02 03:45:29 -0300

    feat: create basic tests for Tag and Cross components

---
+++ components/cross/Cross.test.tsx
@@ -0,0 +1,5 @@
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Cross", () => {
+  test.todo("please pass");
+});
---
+++ components/tag/Tag.test.tsx
@@ -0,0 +1,5 @@
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Tag", () => {
+  test.todo("please pass");
+});
