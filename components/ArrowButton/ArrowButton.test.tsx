97ae3f2bc3f93834ea0f45886033548dc4f9f246
Alejandra De Los Santos <aledeloss@gmail.com>
2023-01-05 23:50:51 -0300

    Agrego tests

---
+++ components/ArrowButton/ArrowButton.test.tsx
@@ -0,0 +1,42 @@
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { ArrowButton } from "./ArrowButton";
+
+describe("Arrow button", () => {
+  it("Component renders without crashing", () => {
+    render(<ArrowButton handleClick={jest.fn()} />);
+  });
+
+  it("Button is correctly aria labeled", () => {
+    const { getByLabelText } = render(<ArrowButton handleClick={jest.fn()} />);
+    expect(getByLabelText("Pasar al slide anterior")).toBeInTheDocument();
+  });
+
+  it("Button is correctly aria labeled", () => {
+    const { getByLabelText } = render(<ArrowButton handleClick={jest.fn()} />);
+    expect(getByLabelText("Pasar al slide anterior")).toBeInTheDocument();
+  });
+
+  it("HandleClick is run on button click", () => {
+    const handleClick = jest.fn();
+    const { getByRole } = render(<ArrowButton handleClick={handleClick} />);
+    const button = getByRole("button");
+    act(() => {
+      fireEvent.click(button);
+    });
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  it("HandleClick is not run click when button is disabled", () => {
+    const handleClick = jest.fn();
+    const { getByRole } = render(
+      <ArrowButton handleClick={handleClick} disabled={true} />
+    );
+    const button = getByRole("button");
+    act(() => {
+      fireEvent.click(button);
+    });
+    expect(handleClick).not.toHaveBeenCalled();
+  });
+});
