8775ccd52bc55f2740911be490fc64244a1a253c
Damian Palavecino <damianpalavecino25@gmail.com>
2023-01-22 17:01:05 -0300

    add XMark tests

--- components/XMark/XMark.test.tsx
+++ components/XMark/XMark.test.tsx
@@ -1,5 +1,15 @@
 import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import XMark from "./XMark";
 
 describe("XMark", () => {
-  test.todo("please pass");
+  it("XMark renders correctly", () => {
+    render(<XMark />);
+  });
+
+  it("XMark component adds className prop", () => {
+    const className = "fill-white";
+    const { container } = render(<XMark className={className} />);
+    expect(container.firstChild).toHaveClass(className);
+  });
 });
