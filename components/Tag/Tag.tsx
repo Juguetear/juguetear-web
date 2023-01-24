31a3cf7269bf0fecbde3ae13c409329337d79c8a
Damian Palavecino <damianpalavecino25@gmail.com>
2023-01-24 19:45:34 -0300

    styles: increase max-width of li and add break-all to span in Tag

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -11,7 +11,7 @@ export const Tag = ({ label, onClick }: TagProps) => {
   }
 
   const liStyle =
-    "flex w-fit max-w-[8.5rem] bg-gray-light text-blue fill-blue rounded-[14px] border-2 border-transparent focus-within:border-white focus-within:shadow-2md";
+    "flex w-fit max-w-[9.5rem] bg-gray-light text-blue fill-blue rounded-[14px] border-2 border-transparent focus-within:border-white focus-within:shadow-2md";
   const buttonStyle =
     "relative peer after:absolute after:content-[''] after:top-[-50%] after:left-[-50%] after:h-[200%] after:w-[200%] px-2 focus:outline-none hover:text-orange hover:fill-orange";
   return (
@@ -24,7 +24,7 @@ export const Tag = ({ label, onClick }: TagProps) => {
       >
         <XMark />
       </button>
-      <span className="peer-hover:text-orange border-l border-gray pl-2 mr-4 font-outfit whitespace-normal">
+      <span className="peer-hover:text-orange border-l border-gray pl-2 mr-4 font-outfit whitespace-normal break-all">
         {label}
       </span>
     </li>
