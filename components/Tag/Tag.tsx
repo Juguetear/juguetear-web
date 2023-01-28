d83a6d1d4ceab6b5dfb1e8c4bb892db91fd9ac54
Damian Palavecino <damianpalavecino25@gmail.com>
2023-01-28 16:22:20 -0300

    change the whole Tag to a button

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -11,22 +11,23 @@ export const Tag = ({ label, onClick }: TagProps) => {
   }
 
   const liStyle =
-    "flex w-fit max-w-[9.5rem] bg-gray-light text-blue fill-blue rounded-[14px] border-2 border-transparent focus-within:border-white focus-within:shadow-2md";
-  const buttonStyle =
-    "relative peer after:absolute after:content-[''] after:top-[-50%] after:left-[-50%] after:h-[200%] after:w-[200%] px-2 focus:outline-none hover:text-orange hover:fill-orange";
+    "w-fit max-w-[9.5rem] bg-gray-light text-blue fill-blue rounded-[14px] border-2 border-transparent focus-within:border-white focus-within:shadow-2md";
+
   return (
     <li className={liStyle}>
       <button
         type="button"
         onClick={onClick}
         aria-label={`Quitar ${label} de la lista`}
-        className={buttonStyle}
+        className="flex items-center focus:outline-none hover:text-orange hover:fill-orange"
       >
-        <XMark />
+        <span className="ml-2 pr-1">
+          <XMark />
+        </span>
+        <span className="border-l border-gray pl-2 ml-1 mr-4 font-outfit whitespace-normal break-all">
+          {label}
+        </span>
       </button>
-      <span className="peer-hover:text-orange border-l border-gray pl-2 mr-4 font-outfit whitespace-normal break-all">
-        {label}
-      </span>
     </li>
   );
 };
