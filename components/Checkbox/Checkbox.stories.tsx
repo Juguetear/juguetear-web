4c551a162abe8d57787775d4478d8d8a6a0e1e0d
Damian Palavecino <damianpalavecino25@gmail.com>
2023-01-31 10:17:11 -0300

    feat: create basic files for the checkbox component

---
+++ components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Checkbox from "./Checkbox";
+
+export default {
+  title: "Checkbox",
+  component: Checkbox,
+} as Meta<typeof Checkbox>;
+
+export const SampleCheckbox: StoryObj<typeof Checkbox> = {
+  args: {},
+};
---
+++ components/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,5 @@
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Checkbox", () => {
+  test.todo("please pass");
+});
