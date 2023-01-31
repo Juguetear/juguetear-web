e50d76a2d108517e94449cfe4dcb2248f9124780
Damian Palavecino <damianpalavecino25@gmail.com>
2023-01-30 22:40:54 -0300

    refactor: change CSF2 to CSF3 in Tag and XMark

--- components/Tag/Tag.stories.tsx
+++ components/Tag/Tag.stories.tsx
@@ -1,54 +1,74 @@
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { Tag, TagProps } from "./Tag";
-
 export default {
   title: "Tag",
   component: Tag,
   argTypes: {
-    onClick: { action: "onClick" },
+    onClick: {
+      action: "onClick",
+    },
     numTags: {
       type: "number",
       defaultValue: 2,
     },
   },
 } as Meta<typeof Tag>;
-
-const Template: StoryFn<typeof Tag> = (args) => (
-  <ul>
-    <Tag {...args} />
-  </ul>
-);
-
-export const SampleTag = Template.bind({});
-SampleTag.storyName = "Sample Tag";
-SampleTag.args = { label: "Movimiento" };
-
-export const LongTag = Template.bind({});
-LongTag.storyName = "Long Tag";
-LongTag.args = { label: "Really long tagname" };
-
-export const TestOnClick = Template.bind({});
-TestOnClick.storyName = "Test On Click";
-TestOnClick.args = {
-  label: "On Click",
-  onClick: () => alert("Button clicked"),
+export const SampleTag: StoryObj<typeof Tag> = {
+  render: (args) => (
+    <ul>
+      <Tag {...args} />
+    </ul>
+  ),
+  name: "Sample Tag",
+  args: {
+    label: "Movimiento",
+  },
 };
-
+export const LongTag: StoryObj<typeof Tag> = {
+  render: (args) => (
+    <ul>
+      <Tag {...args} />
+    </ul>
+  ),
+  name: "Long Tag",
+  args: {
+    label: "Really long tagname",
+  },
+};
+export const TestOnClick: StoryObj<typeof Tag> = {
+  render: (args) => (
+    <ul>
+      <Tag {...args} />
+    </ul>
+  ),
+  name: "Test On Click",
+  args: {
+    label: "On Click",
+    onClick: () => alert("Button clicked"),
+  },
+};
 interface MultipleTagsProps extends TagProps {
   numTags: number;
 }
-
-const MultiTagsTemplate: StoryFn<MultipleTagsProps> = (args) => {
-  const { numTags } = args;
-  return (
-    <ul className="flex">
-      {Array.from({ length: numTags }, (_, i) => (
-        <Tag key={i} {...args} />
-      ))}
-    </ul>
-  );
+export const MultipleTags: StoryObj<MultipleTagsProps> = {
+  render: (args) => {
+    const { numTags } = args;
+    return (
+      <ul className="flex">
+        {Array.from(
+          {
+            length: numTags,
+          },
+          (_, i) => (
+            <Tag key={i} {...args} />
+          )
+        )}
+      </ul>
+    );
+  },
+  name: "Multiple Tags",
+  args: {
+    label: "Tag",
+    numTags: 2,
+  },
 };
-
-export const MultipleTags = MultiTagsTemplate.bind({});
-MultipleTags.storyName = "Multiple Tags";
-MultipleTags.args = { label: "Tag", numTags: 2 };
--- components/XMark/XMark.stories.tsx
+++ components/XMark/XMark.stories.tsx
@@ -1,15 +1,14 @@
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import XMark from "./XMark";
-
 export default {
   title: "Iconos/XMark",
   component: XMark,
 } as Meta<typeof XMark>;
-
-const Template: StoryFn<typeof XMark> = (args) => <XMark {...args} />;
-
-export const DefaultXMark = Template.bind({});
-DefaultXMark.args = {};
-
-export const WhiteXMark = Template.bind({});
-WhiteXMark.args = { className: "fill-white" };
+export const DefaultXMark: StoryObj<typeof XMark> = {
+  args: {},
+};
+export const WhiteXMark: StoryObj<typeof XMark> = {
+  args: {
+    className: "fill-white",
+  },
+};
