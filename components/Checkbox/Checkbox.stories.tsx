f02c18c2b074df6a9d620e4c14ae6435bbf799e2
Damian Palavecino <damianpalavecino25@gmail.com>
2023-02-03 19:53:12 -0300

    feat: create Checkbox component stories

--- components/Checkbox/Checkbox.stories.tsx
+++ components/Checkbox/Checkbox.stories.tsx
@@ -1,13 +1,76 @@
 import { Meta, StoryObj } from "@storybook/react";
-import Checkbox from "./Checkbox";
+import { Checkbox, CheckboxProps } from "./Checkbox";
 
 export default {
   title: "Checkbox",
   component: Checkbox,
+  argTypes: {
+    checked: {
+      control: "boolean",
+    },
+    disabled: {
+      control: "boolean",
+    },
+    label: {
+      control: "text",
+    },
+    numberOfCheckboxes: {
+      type: "number",
+      defaultValue: 2,
+    },
+  },
 } as Meta<typeof Checkbox>;
 
 export const SampleCheckbox: StoryObj<typeof Checkbox> = {
   args: {
     label: "Checkbox",
+  },
+};
+
+export const Checked: StoryObj<typeof Checkbox> = {
+  args: {
+    checked: true,
+    label: "Checked",
+  },
+};
+
+export const Unchecked: StoryObj<typeof Checkbox> = {
+  args: {
+    checked: false,
+    label: "Unchecked",
+  },
+};
+
+export const Disabled: StoryObj<typeof Checkbox> = {
+  args: {
+    checked: false,
+    disabled: true,
+    label: "Disabled",
+  },
+};
+
+interface MultipleCheckboxProps extends CheckboxProps {
+  numberOfCheckboxes: number;
+}
+export const MultipleCheckbox: StoryObj<MultipleCheckboxProps> = {
+  render: (args) => {
+    const { numberOfCheckboxes } = args;
+    return (
+      <ul className="flex flex-col justify-evenly">
+        {Array.from(
+          {
+            length: numberOfCheckboxes,
+          },
+          (_, i) => (
+            <Checkbox key={i} label={`Checkbox ${i}`} />
+          )
+        )}
+      </ul>
+    );
+  },
+  name: "Multiple Checkboxes",
+  args: {
+    label: "Checkbox",
+    numberOfCheckboxes: 2,
   },
 };
