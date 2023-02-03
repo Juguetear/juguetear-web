5bb347dd200be8b6d8307c27dcdd55175636d972
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2023-02-03 04:07:41 +0000

    StoryFn removed from the Accordion story

--- components/Accordion/Accordion.stories.tsx
+++ components/Accordion/Accordion.stories.tsx
@@ -1,6 +1,16 @@
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta } from "@storybook/react";
 
 import { Accordion } from "./Accordion";
+
+interface ArgsProps {
+  "Accordion 1": AccordionProps;
+  "Accordion 2": AccordionProps;
+}
+
+type AccordionProps = {
+  title: string;
+  children: JSX.Element;
+};
 
 export default {
   title: "Accordion",
@@ -14,24 +24,24 @@ export const Normal = {
   },
 };
 
-const TemplateSet: StoryFn = (args) => {
-  return (
-    <>
-      <Accordion {...args["Accordion 1"]} />
-      <Accordion {...args["Accordion 2"]} />
-    </>
-  );
-};
-
-export const Conjunto = TemplateSet.bind({});
-
-Conjunto.args = {
-  "Accordion 1": {
-    title: "¿Hay que pagar para usar los juguetes?",
-    children: <p>No, el prestamo es sin costo alguno.</p>,
+export const Conjunto = {
+  render: (args: ArgsProps) => {
+    return (
+      <>
+        <Accordion {...args["Accordion 1"]} />
+        <Accordion {...args["Accordion 2"]} />
+      </>
+    );
   },
-  "Accordion 2": {
-    title: "¿Envían a otros lugares fuera de La Plata?",
-    children: <p>No, el prestamo es solo en La Plata.</p>,
+
+  args: {
+    "Accordion 1": {
+      title: "¿Hay que pagar para usar los juguetes?",
+      children: <p>No, el prestamo es sin costo alguno.</p>,
+    },
+    "Accordion 2": {
+      title: "¿Envían a otros lugares fuera de La Plata?",
+      children: <p>No, el prestamo es solo en La Plata.</p>,
+    },
   },
 };
