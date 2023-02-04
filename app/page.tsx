ff6ae373e10f3ced2438cdfe3d7c16f180c90166
TonyMckes <tonymckes@gmail.com>
2023-02-04 07:11:35 -0400

    feat: add under construction page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,57 @@
+import { Logo } from "components/Logo/Logo";
 import Image from "next/image";
 
 function Home() {
+  const StyledLink = ({
+    href,
+    children,
+  }: {
+    href: string;
+    children: React.ReactNode;
+  }) => {
+    return (
+      <a
+        className="font-bold text-blue hover:text-darkblue"
+        href={href}
+        rel="noreferrer"
+        target="_blank"
+      >
+        {children}
+      </a>
+    );
+  };
+
   return (
-    <div>
-      <main>
-        <h1>Hola!</h1>
+    <div className="flex flex-col min-h-screen justify-evenly bg-blue-light ">
+      <main className="px-4 mx-auto space-y-5 max-w-prose">
+        <Logo className="mx-auto" />
+        <h1 className="text-center">¡Sitio web en construcción!</h1>
+        <p>
+          Si deseas saber mas sobre la iniciativa, visita nuestro{" "}
+          <StyledLink href="https://github.com/Juguetear/juguetear-web#readme">
+            README.
+          </StyledLink>{" "}
+          Y si deseas contribuir con este proyecto, por favor, ve a la sección
+          sobre{" "}
+          <StyledLink href="https://github.com/Juguetear/juguetear-web#c%C3%B3mo-contribuir">
+            cómo contribuir
+          </StyledLink>{" "}
+          para poder hacerlo.
+        </p>
+        <p>
+          Si en necesitas hacer consultas, podés hacerlas en nuestro canal{" "}
+          <StyledLink href="https://discord.com/channels/594363964499165194/1035685740409012244">
+            #juguetear
+          </StyledLink>{" "}
+          en Discord.
+        </p>
+        <p className="px-4 py-6 rounded bg-black/10">
+          ⚠️ Por favor, leer y adherir al{" "}
+          <StyledLink href="https://github.com/Juguetear/juguetear-web/blob/development/CODE_OF_CONDUCT.md">
+            código de conducta
+          </StyledLink>{" "}
+          de este proyecto.
+        </p>
       </main>
 
       <footer>
