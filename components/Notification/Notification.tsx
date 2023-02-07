c3c7fa39930355f26abed34791f6e0c4760edf59
Alejandra De Los Santos <aledeloss@gmail.com>
2023-02-06 23:24:09 -0300

    Add conditional styling when has icon

--- components/Notification/Notification.tsx
+++ components/Notification/Notification.tsx
@@ -1,6 +1,17 @@
-export const Notification = ({ children }: { children: JSX.Element }) => {
+interface NotificationProps {
+  children: JSX.Element;
+  hasIcon?: boolean;
+}
+export const Notification = ({
+  children,
+  hasIcon = false,
+}: NotificationProps) => {
   return (
-    <div className="bg-turquoise/[.1] rounded-lg p-4 m-4 flex justify-center">
+    <div
+      className={`flex justify-between bg-turquoise/[0.1] rounded-lg py-8 px-[83px] ${
+        hasIcon ? " pl-[7px]" : " pl-[83px]"
+      }`}
+    >
       {children}
     </div>
   );
