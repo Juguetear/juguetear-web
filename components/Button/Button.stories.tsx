2234835d99b3c88322d4d6c8f0594f3b247bdadb
beetlejuice96 <dilanbernbae043@gmail.com>
2023-02-14 21:41:31 -0300

    apply suggestions

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryFn } from "@storybook/react";
+import Image from "next/image";
 import PdfSvg from "../assets/pdf.svg";
 import { Button } from "./Button";
 
@@ -6,7 +7,7 @@ export default {
   title: "Button",
   component: Button,
   argTypes: {
-    handleClick: {
+    onClick: {
       action: "Clicked",
     },
   },
@@ -17,31 +18,37 @@ const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
 export const button = Template.bind({});
 button.args = {
   appearance: "button",
-  text: "button",
+  children: <>button</>,
 };
 
 export const simpleButton = Template.bind({});
 simpleButton.args = {
   appearance: "button",
-  text: "button",
+  children: <>button</>,
 };
 
 export const link = Template.bind({});
 link.args = {
   appearance: "link",
-  text: "link",
+  children: <>link</>,
 };
 
 export const buttonWithIcon = Template.bind({});
 buttonWithIcon.args = {
   appearance: "buttonWithIcon",
-  text: "buttonWithIcon",
-  icon: PdfSvg,
+  children: (
+    <>
+      <div className="pr-3">
+        <Image src={PdfSvg} alt="SVG" width={27} height={35} />
+      </div>
+      buttonWithIcon
+    </>
+  ),
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
-  disabled: true,
   appearance: "button",
-  text: "button disabled",
+  disabled: true,
+  children: <>button disabled</>,
 };
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,53 +1,31 @@
-import Image from "next/image";
-import React, { MouseEventHandler } from "react";
-interface Appearance {
-  link: string;
-  buttonWithIcon: string;
-  button: string;
-}
+import React from "react";
 
-const BASE_STYLE =
+const baseStyle =
   "font-extrabold rounded focus-within:shadow-md shadow-turquoise text-base font-outfit";
 
-const BASE_STYLE_LINK = "py-1 px-2 text-blue focus:text-blue underline";
+const baseStyleLink = "py-1 px-2 text-blue focus:text-blue underline";
 
-const APPEARANCE: Appearance = {
-  link: `${BASE_STYLE} ${BASE_STYLE_LINK} hover:text-orange`,
-  buttonWithIcon: `${BASE_STYLE} flex items-center ${BASE_STYLE_LINK} "hover:text-orange"`,
-  button: `${BASE_STYLE} bg-orange focus:bg-orange  py-3 px-7 text-white hover:bg-blue`,
+const Appearance = {
+  link: `${baseStyle} ${baseStyleLink} hover:text-orange`,
+  buttonWithIcon: `${baseStyle} flex items-center ${baseStyleLink} hover:text-orange`,
+  button: `${baseStyle} bg-orange focus:bg-orange  py-3 px-7 text-white hover:bg-blue`,
 };
 
-type Appearances = keyof Appearance;
+type Appearances = keyof typeof Appearance;
 
-interface ButtonProps
-  extends React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {
-  disabled?: boolean;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   appearance: Appearances;
-  handleClick: MouseEventHandler;
-  buttonAttributes?: Partial<HTMLButtonElement>;
-  icon?: string;
-  text: string;
 }
 
 export const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
-  const { appearance, text, disabled, icon, handleClick, ...rest } = props;
+  const { appearance, children, ...rest } = props;
 
   return (
     <button
-      disabled={disabled}
-      className={`${APPEARANCE[appearance]} disabled:cursor-not-allowed disabled:bg-gray`}
-      onClick={handleClick}
       {...rest}
+      className={`${Appearance[appearance]} disabled:cursor-not-allowed disabled:bg-gray`}
     >
-      {icon && (
-        <div className="pr-3">
-          <Image src={icon} alt="SVG" width={27} height={35} />
-        </div>
-      )}
-      {text}
+      {children}
     </button>
   );
 };
