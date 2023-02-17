f1cf26f35d568874c1bb3b0308183c7a4e3c4f37
Damian Palavecino <damianpalavecino25@gmail.com>
2023-02-17 09:03:09 -0300

    fix: change CheckboxProps to an interface and update component props

--- components/Checkbox/Checkbox.tsx
+++ components/Checkbox/Checkbox.tsx
@@ -1,20 +1,13 @@
-export type CheckboxProps = {
-  checked?: boolean;
-  disabled?: boolean;
+interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: string;
   label: string;
-};
+}
 
-export const Checkbox = ({
-  checked = false,
-  label,
-  disabled,
-}: CheckboxProps) => {
+export const Checkbox = ({ label, id, ...props }: CheckboxProps) => {
   const inputStyle =
     "peer appearance-none w-4 min-w-[1.25rem] h-5 mt-[1.5px] border rounded-[2px] border-blue cursor-pointer checked:border-orange checked:bg-orange hover:enabled:border-2 focus:outline-none disabled:cursor-not-allowed disabled:border-gray-dark disabled:bg-gray-medium";
 
-  const formatLabel = (label: string) => {
-    return label.toLowerCase().split(" ").join("");
-  };
+  const formattedId = id.toLowerCase().split(" ").join("");
 
   if (!label || label.trim().length === 0) {
     return null;
@@ -23,15 +16,13 @@ export const Checkbox = ({
   return (
     <div className="flex max-w-[13rem] focus-within:shadow-2md p-1 rounded">
       <input
-        id={formatLabel(label)}
+        id={formattedId}
         type="checkbox"
-        checked={checked}
-        aria-checked={checked}
         className={inputStyle}
-        disabled={disabled}
+        {...props}
       />
       <label
-        htmlFor={formatLabel(label)}
+        htmlFor={formattedId}
         className="text-blue text-sm font-outfit ml-2 peer-checked:text-orange font-medium peer-hover:font-bold peer-disabled:text-gray-dark peer-disabled:font-medium"
       >
         {label}
