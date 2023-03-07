4e0531ebf6bad6e22f67b5034e0cc03536669d1a
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2023-03-07 10:54:02 +0000

    Refactor: Tag.test.tsx

--- components/Tag/Tag.test.tsx
+++ components/Tag/Tag.test.tsx
@@ -1,41 +1,43 @@
-import "@testing-library/jest-dom/extend-expect";
-import { fireEvent, render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Tag } from "./Tag";
 
 describe("Tag", () => {
   const label = "Example label";
   const onClick = jest.fn();
 
-  test("renders the label and XMark button", () => {
-    const { getByText, getByRole } = render(
-      <Tag label={label} onClick={onClick} />
-    );
-    const span = getByText(label);
-    const button = getByRole("button");
+  it("renders the label and XMark button", () => {
+    render(<Tag label={label} onClick={onClick} />);
+    const span = screen.getByText(label);
+    const button = screen.getByRole("button");
 
     expect(span).toBeInTheDocument();
     expect(button).toBeInTheDocument();
   });
 
-  test("calls the onClick function when the button is clicked", () => {
-    const { getByRole } = render(<Tag label={label} onClick={onClick} />);
-    const button = getByRole("button");
+  it("calls the onClick function when the button is clicked", async () => {
+    render(<Tag label={label} onClick={onClick} />);
+    const button = await screen.findByRole("button");
 
-    fireEvent.click(button);
+    await userEvent.click(button);
+
     expect(onClick).toHaveBeenCalled();
   });
 
-  test("sets the correct aria-label on the button", () => {
-    const { getByRole } = render(<Tag label={label} onClick={onClick} />);
-    const button = getByRole("button");
+  it("sets the correct aria-label on the button", () => {
+    render(<Tag label={label} onClick={onClick} />);
+    const button = screen.getByRole("button", {
+      name: `Quitar ${label} de la lista`,
+    });
     expect(button).toHaveAttribute("aria-label", `Quitar ${label} de la lista`);
   });
 
-  test.each(["", "   "])(
+  it.each(["", "   "])(
     "should return null if label is empty or full of spaces",
     (label) => {
-      const { container } = render(<Tag label={label} onClick={onClick} />);
-      expect(container.firstChild).toBeNull();
+      render(<Tag label={label} onClick={onClick} />);
+      const text = screen.queryByText(`Quitar ${label} de la lista`);
+      expect(text).not.toBeInTheDocument();
     }
   );
 });
