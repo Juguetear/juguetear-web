be7b5878cebaa2a4a346967ca674a03f3d4e3007
Adrian Andoro <adrian.a@ensolvers.com>
2023-03-08 14:32:10 -0300

    cambiar el focus-within por focus-visible para que tome solo el teclado

--- components/Checkbox/Checkbox.tsx
+++ components/Checkbox/Checkbox.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 export interface CheckboxProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
@@ -7,9 +5,7 @@ export interface CheckboxProps
 }
 
 export const Checkbox = ({ label, id, ...props }: CheckboxProps) => {
-  const [focus, setFocus] = useState<string>("");
-
-  const containerStyle = "flex max-w-[13rem] items-center rounded p-1";
+  const containerStyle = "flex max-w-[13rem] items-center rounded p-1 ";
 
   const inputStyle =
     "peer appearance-none w-5 min-w-[1.25rem] h-5 border rounded-[2px] border-blue cursor-pointer checked:border-orange checked:bg-orange hover:enabled:border-2 focus:outline-none disabled:cursor-not-allowed disabled:border-gray-dark disabled:bg-gray-medium";
@@ -23,27 +19,18 @@ export const Checkbox = ({ label, id, ...props }: CheckboxProps) => {
     return null;
   }
 
-  const handlerOnClick = () => {
-    setFocus("");
-  };
-
-  const handlerOnFocus = () => {
-    setFocus("focus-within:shadow-2md");
-  };
-
   return (
-    <div className={`${containerStyle} ${focus}`}>
+    <div className={containerStyle}>
       <input
         id={formattedId}
         type="checkbox"
         className={inputStyle}
         {...props}
-        onClick={handlerOnClick}
-        onFocus={handlerOnFocus}
       />
       <label htmlFor={formattedId} className={labelStyle}>
         {label}
       </label>
+      <div className="absolute max-w-full focus-visible:shadow-2md"></div>
     </div>
   );
 };
