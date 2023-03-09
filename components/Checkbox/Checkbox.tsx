af339e2ee3c25a6400a512e858946d9dd555992e
Adrian Andoro <adrian.a@ensolvers.com>
2023-03-09 09:17:35 -0300

    FIX los z-index en label, input y div

--- components/Checkbox/Checkbox.tsx
+++ components/Checkbox/Checkbox.tsx
@@ -8,10 +8,10 @@ export const Checkbox = ({ label, id, ...props }: CheckboxProps) => {
   const containerStyle = "flex relative max-w-[13rem] items-center rounded p-1";
 
   const inputStyle =
-    "peer z-40 appearance-none w-5 min-w-[1.25rem] h-5 border rounded-[2px] border-blue cursor-pointer checked:border-orange checked:bg-orange hover:enabled:border-2 focus:outline-none disabled:cursor-not-allowed disabled:border-gray-dark disabled:bg-gray-medium";
+    "peer z-10 appearance-none w-5 min-w-[1.25rem] h-5 border rounded-[2px] border-blue cursor-pointer checked:border-orange checked:bg-orange hover:enabled:border-2 focus:outline-none disabled:cursor-not-allowed disabled:border-gray-dark disabled:bg-gray-medium";
 
   const labelStyle =
-    "z-30 text-blue text-sm font-outfit ml-2 peer-checked:text-orange font-medium peer-hover:font-bold peer-disabled:text-gray-dark peer-disabled:font-medium";
+    "z-10 text-blue text-sm font-outfit ml-2 peer-checked:text-orange font-medium peer-hover:font-bold peer-disabled:text-gray-dark peer-disabled:font-medium";
 
   const formattedId = id.toLowerCase().split(" ").join("");
 
@@ -30,7 +30,7 @@ export const Checkbox = ({ label, id, ...props }: CheckboxProps) => {
       <label htmlFor={formattedId} className={labelStyle}>
         {label}
       </label>
-      <div className="absolute inset-0 z-20 rounded peer-focus-visible:shadow-2md"></div>
+      <div className="absolute inset-0 rounded peer-focus-visible:shadow-2md"></div>
     </div>
   );
 };
