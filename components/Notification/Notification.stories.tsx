7187d45cbb8193a054934229dd376d460aa519c9
Damian Palavecino <damianpalavecino25@gmail.com>
2023-03-17 17:02:05 -0300

    refactor: update stories to CSF3

--- components/Notification/Notification.stories.tsx
+++ components/Notification/Notification.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import EnvelopeIcon from "../assets/EnvelopeIcon";
 import Notification from "./Notification";
 
@@ -7,29 +7,27 @@ export default {
   component: Notification,
 } as Meta<typeof Notification>;
 
-const Template: StoryFn<typeof Notification> = (args) => (
-  <Notification {...args} />
-);
-
-export const Default = Template.bind({});
-Default.args = {
-  children: (
-    <p>
-      No tenemos juguetes disponibles en esta categoría. Volvé a intentarlo más
-      tarde o mirá todos los juguetes disponibles.
-    </p>
-  ),
+export const Default: StoryObj = {
+  args: {
+    children: (
+      <p>
+        No tenemos juguetes disponibles en esta categoría. Volvé a intentarlo
+        más tarde o mirá todos los juguetes disponibles.
+      </p>
+    ),
+  },
 };
 
-export const NotificationWithIcon = Template.bind({});
-NotificationWithIcon.args = {
-  children: (
-    <>
-      <EnvelopeIcon />
-      <div className="ml-5 text-center">
-        <p>¡Tu mensaje fue enviado! Te responderemos a la brevedad.</p>
-        <p className="font-semibold">Enviar otro mensaje</p>
-      </div>
-    </>
-  ),
+export const NotificationWithIcon: StoryObj = {
+  args: {
+    children: (
+      <>
+        <EnvelopeIcon />
+        <div className="ml-5 text-center">
+          <p>¡Tu mensaje fue enviado! Te responderemos a la brevedad.</p>
+          <p className="font-semibold">Enviar otro mensaje</p>
+        </div>
+      </>
+    ),
+  },
 };
