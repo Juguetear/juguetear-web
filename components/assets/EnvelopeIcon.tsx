770f3c7a4f10db466ff9934bb3769a515f83a4f4
Damian Palavecino <damianpalavecino25@gmail.com>
2023-03-17 16:43:05 -0300

    feat: add Envelope icon to assets folder

---
+++ components/assets/EnvelopeIcon.tsx
@@ -0,0 +1,66 @@
+const EnvelopeIcon = () => {
+  return (
+    <svg
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      width="56px"
+      height="56px"
+      viewBox="0 0 56 56"
+      enable-background="new 0 0 56 56"
+      xmlSpace="preserve"
+    >
+      <image
+        id="image0"
+        width="56"
+        height="56"
+        x="0"
+        y="0"
+        href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAA4CAYAAACohjseAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+        AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QA/wD/AP+gvaeTAAAA
+        CXBIWXMAAAsTAAALEwEAmpwYAAAHfUlEQVRo3u2Ze2xT1x3HP762EyckxGnIg9TBjgOtmpLFVFRT
+        0egotNM67aGNVVq7stIWIe2hsu7BhljDJli2MUroWLf1DyDVOqRVe8FYKQzUsgdaW6aZJoRmCYmd
+        OE2TYrAdYsdxbO+Pa/vaN/f6hSn7w1/Jyjn3ntfn/s75/c45gaKKKqqooopSlfZmDyDPMd8K6IFA
+        psKamz3aNKoBrEAbsDSWbgUsQGWszJvA08DZ/1fAGqAWWBVLtwE2GUQmhYFPA6/cLEAtYI4NvCU2
+        +BZEazQVqI+xWB/hGwUYh2gBmoHlsXRbvhBaDZhvAZsJLLeIvzed8NI51So24Lz8oS7HfrNZFzlB
+        GMtgWR20NoDtVqirgFVW8XllqVT2rRHYfkzKm+v1CIKG4fHZnAEXIHqpj5D/ukj9KgvAWgNtjWCp
+        Fq1hM4GlJhVCTefH4MFfwlRQzDfV6TnV1czOFyeTAS1KdeWAm4Afx8ByUolOg7lBQ5tFh7VRYLlF
+        S71RQ1uzjsULQRj05PNtcFyBT7wAbr+YrzNqOdJpxtxQQvvSMjiRaDcj4FPAc+k60wpgrhewtegw
+        1wtY6gWsjQIfatZhqhXyAsgEt2Y/vOsV81UVAn/+iYXlVgMASxr0ycVt6QAXALvkL2sWCnQ8aqCu
+        WrSMqVagsjxPv6QXIBTJuviYV4RzXhHzhhINhzuWsGJZmUTUYkiuYlFqJ/7Z70Zhfbl9Ebp+P0N1
+        hcAdZm3+cEDUkL0/c0/Dvc9JcBoNPP+NRh5YWZFSztxQkpw1xn6KgNXxB1WVeswm6Ss5JiJ8fNsU
+        j+2exjGRvQXmSZfdFHZPwwO/gCG39OynX25gw8eqFcubU6epRf4+vhedAb4OUGbQMvyPdfgDYc69
+        7SESFQu8PRRm/59mCEegrVlHuSE3a2rmImiuhdKWCYVh/UH455D0bMfGer79cK1qnb/Z/fSPBuPZ
+        fyELFfHP6ok/8PhCBGcj7O24k4Eza1n/4OJE4WgUdv0mwMqveHnxZJBcFNVntuDnDsDJd6T89g11
+        bP9Sbdo65gyOJhnQEX/oGBV9sqWpnN/9aiVHD9xNc1N5otLo+xGe2DNN+2Yvb1ycy46wJP3BZcsf
+        4NgFKf+tLyxix+N1GZu1zF+HioAA9kSiz5dS6FP3NzBwZi17n2mlrkaKzL2OMKu2+Hjy2WmckxnW
+        p14AQXlaP/MX+NkZKf/dL9bSubkhq+8mCxVr0gE6EgmXf15DWq2Gpze1cO7YajY/siTlXfeJILbN
+        Xn54OIA/GFUdjNI0/f5x2HVSyj+8rortGzJbLi5ZqDBmBehUAIyrqbGMF37UTv9r93HPXVJ7Pn+U
+        ju4AbZu8/Pb1WeXKslCx8wT84FUp/8l7KjmwzURpSfYOzNxQgrEiMf2NiJv+9IDyKaqk26wVnP3j
+        an69b8W8sPJI5zXu3+rjvy7ZtE2y4O7T0JF0gmuzlnJwmwmdkHusrapImRk2NcCEe3W4Mt4EJPTo
+        Z01cPH0fO795O+UGyZG8Zp+j9UkPm/ZOM+aOpAB2vwHfOSq1scxUypFOS7IlclJ7S1ly1qIG6Ign
+        PL6Q4jpUU5lBy/eeuo0Lp9aw8SHp+BeNwqFXg3z4az5+fmSGqEHHqX54/LBU11yv55XdZkx1+qz7
+        kytdsJev+gSkvc+bc0eWpnIO7bHxn+P3Yl0ihZVxd4Qtz/tpecLH+oOpcH/tapZvuXJW+1L1Pakc
+        0B5PeLxZxjcF2VqruPT3dRzaY0tdn5ej+GbEtKlWx6muZnkcyw8wdYra0gE6EqRZOJpM2vhQE28d
+        Xc22ry5Fp5Ocx6KFWk7vs1635eLKZYomOZrs12A61daU0rn1DgZeX8tjnzfx0bsqOb3PSvPiwsAB
+        GCu0cgeVCBXyM8xwgrQAFkyWpamc7mdXEPZ4ibivFLRtEHc0nsHEpZoNcMJ8CzrjiUJZUC6NUPiT
+        P6gffpXWoOdGQmrKyq6/EQXJ1rNNDTAOCeQXKjJKe2MsqOZolHqzJ0hHs9/RZCuNIKDR5Xodm1lq
+        u5m0Fsxly5aTSgrnQeOqqUrxoontlBLgjXc0+sJa0DUZouvly4oMSj3Z44lCh4qE8pii4+45eodn
+        mLw6R+9wkOF3Z+kZCjAyESIYmncGfSkdoCORcPnx+EIYF+a/EVaSmgWn/BFGJkLYBwM43wvhfG+W
+        XscMQ2Mh3L6st459wI50gJ7Yzwji/YztzqqCwU1NzzE6Msv5s14mr4Y5Pxjg4kiQgdFZPNfChCPR
+        vJoFLgDHga7kF2pzxUEsltj7fHkBusZn6On3MeiYxuHy43QFsF/w4hgLEA7nBQHgAnqAQaA3Ns5L
+        sb9hpQpqgPY44OWrKtcPwPhkkJ5+HxPvB+npn2LIOU3POz6crgDBHK7pFSD6YgMfBoZi41GFSCc1
+        wH8DGwH2dw9ze0sFHm8Ip8uPwxWgp9/HJacfdxr4DJqKDdiOaJEriP9nnwTc+TaqJLULECMwACy6
+        jrbjEBcRrXEpBnOp0BD5AAJ8BngZyBSVc14XH6QyXWFZgK2I/3O/Fhv0da+Loooqqqiiiirqg9H/
+        ADXHZvFxisQUAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIzLTAzLTE3VDE0OjIwOjU0KzAwOjAwVZhm
+        ewAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMy0wMy0xN1QxNDoyMDo1NCswMDowMCTF3scAAAAodEVY
+        dGRhdGU6dGltZXN0YW1wADIwMjMtMDMtMTdUMTQ6MjA6NTQrMDA6MDBz0P8YAAAAAElFTkSuQmCC"
+      />
+    </svg>
+  );
+};
+
+export default EnvelopeIcon;
