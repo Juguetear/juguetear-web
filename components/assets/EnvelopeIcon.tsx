915b33dc695e46414dcfae71530f6530e5da57d4
Damian Palavecino <damianpalavecino25@gmail.com>
2023-03-17 21:18:18 -0300

    change Envelope Icon SVG to make it more compact

--- components/assets/EnvelopeIcon.tsx
+++ components/assets/EnvelopeIcon.tsx
@@ -1,63 +1,89 @@
 const EnvelopeIcon = () => {
   return (
     <svg
-      version="1.1"
-      id="Layer_1"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      x="0px"
-      y="0px"
-      width="56px"
-      height="56px"
-      viewBox="0 0 56 56"
-      enable-background="new 0 0 56 56"
-      xmlSpace="preserve"
+      width={50}
+      height={50}
+      viewBox="0 0 142.004 109.214"
     >
-      <image
-        id="image0"
-        width="56"
-        height="56"
-        x="0"
-        y="0"
-        href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAA4CAYAAACohjseAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
-        AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QA/wD/AP+gvaeTAAAA
-        CXBIWXMAAAsTAAALEwEAmpwYAAAHfUlEQVRo3u2Ze2xT1x3HP762EyckxGnIg9TBjgOtmpLFVFRT
-        0egotNM67aGNVVq7stIWIe2hsu7BhljDJli2MUroWLf1DyDVOqRVe8FYKQzUsgdaW6aZJoRmCYmd
-        OE2TYrAdYsdxbO+Pa/vaN/f6hSn7w1/Jyjn3ntfn/s75/c45gaKKKqqooopSlfZmDyDPMd8K6IFA
-        psKamz3aNKoBrEAbsDSWbgUsQGWszJvA08DZ/1fAGqAWWBVLtwE2GUQmhYFPA6/cLEAtYI4NvCU2
-        +BZEazQVqI+xWB/hGwUYh2gBmoHlsXRbvhBaDZhvAZsJLLeIvzed8NI51So24Lz8oS7HfrNZFzlB
-        GMtgWR20NoDtVqirgFVW8XllqVT2rRHYfkzKm+v1CIKG4fHZnAEXIHqpj5D/ukj9KgvAWgNtjWCp
-        Fq1hM4GlJhVCTefH4MFfwlRQzDfV6TnV1czOFyeTAS1KdeWAm4Afx8ByUolOg7lBQ5tFh7VRYLlF
-        S71RQ1uzjsULQRj05PNtcFyBT7wAbr+YrzNqOdJpxtxQQvvSMjiRaDcj4FPAc+k60wpgrhewtegw
-        1wtY6gWsjQIfatZhqhXyAsgEt2Y/vOsV81UVAn/+iYXlVgMASxr0ycVt6QAXALvkL2sWCnQ8aqCu
-        WrSMqVagsjxPv6QXIBTJuviYV4RzXhHzhhINhzuWsGJZmUTUYkiuYlFqJ/7Z70Zhfbl9Ebp+P0N1
-        hcAdZm3+cEDUkL0/c0/Dvc9JcBoNPP+NRh5YWZFSztxQkpw1xn6KgNXxB1WVeswm6Ss5JiJ8fNsU
-        j+2exjGRvQXmSZfdFHZPwwO/gCG39OynX25gw8eqFcubU6epRf4+vhedAb4OUGbQMvyPdfgDYc69
-        7SESFQu8PRRm/59mCEegrVlHuSE3a2rmImiuhdKWCYVh/UH455D0bMfGer79cK1qnb/Z/fSPBuPZ
-        fyELFfHP6ok/8PhCBGcj7O24k4Eza1n/4OJE4WgUdv0mwMqveHnxZJBcFNVntuDnDsDJd6T89g11
-        bP9Sbdo65gyOJhnQEX/oGBV9sqWpnN/9aiVHD9xNc1N5otLo+xGe2DNN+2Yvb1ycy46wJP3BZcsf
-        4NgFKf+tLyxix+N1GZu1zF+HioAA9kSiz5dS6FP3NzBwZi17n2mlrkaKzL2OMKu2+Hjy2WmckxnW
-        p14AQXlaP/MX+NkZKf/dL9bSubkhq+8mCxVr0gE6EgmXf15DWq2Gpze1cO7YajY/siTlXfeJILbN
-        Xn54OIA/GFUdjNI0/f5x2HVSyj+8rortGzJbLi5ZqDBmBehUAIyrqbGMF37UTv9r93HPXVJ7Pn+U
-        ju4AbZu8/Pb1WeXKslCx8wT84FUp/8l7KjmwzURpSfYOzNxQgrEiMf2NiJv+9IDyKaqk26wVnP3j
-        an69b8W8sPJI5zXu3+rjvy7ZtE2y4O7T0JF0gmuzlnJwmwmdkHusrapImRk2NcCEe3W4Mt4EJPTo
-        Z01cPH0fO795O+UGyZG8Zp+j9UkPm/ZOM+aOpAB2vwHfOSq1scxUypFOS7IlclJ7S1ly1qIG6Ign
-        PL6Q4jpUU5lBy/eeuo0Lp9aw8SHp+BeNwqFXg3z4az5+fmSGqEHHqX54/LBU11yv55XdZkx1+qz7
-        kytdsJev+gSkvc+bc0eWpnIO7bHxn+P3Yl0ihZVxd4Qtz/tpecLH+oOpcH/tapZvuXJW+1L1Pakc
-        0B5PeLxZxjcF2VqruPT3dRzaY0tdn5ej+GbEtKlWx6muZnkcyw8wdYra0gE6EqRZOJpM2vhQE28d
-        Xc22ry5Fp5Ocx6KFWk7vs1635eLKZYomOZrs12A61daU0rn1DgZeX8tjnzfx0bsqOb3PSvPiwsAB
-        GCu0cgeVCBXyM8xwgrQAFkyWpamc7mdXEPZ4ibivFLRtEHc0nsHEpZoNcMJ8CzrjiUJZUC6NUPiT
-        P6gffpXWoOdGQmrKyq6/EQXJ1rNNDTAOCeQXKjJKe2MsqOZolHqzJ0hHs9/RZCuNIKDR5Xodm1lq
-        u5m0Fsxly5aTSgrnQeOqqUrxoontlBLgjXc0+sJa0DUZouvly4oMSj3Z44lCh4qE8pii4+45eodn
-        mLw6R+9wkOF3Z+kZCjAyESIYmncGfSkdoCORcPnx+EIYF+a/EVaSmgWn/BFGJkLYBwM43wvhfG+W
-        XscMQ2Mh3L6st459wI50gJ7Yzwji/YztzqqCwU1NzzE6Msv5s14mr4Y5Pxjg4kiQgdFZPNfChCPR
-        vJoFLgDHga7kF2pzxUEsltj7fHkBusZn6On3MeiYxuHy43QFsF/w4hgLEA7nBQHgAnqAQaA3Ns5L
-        sb9hpQpqgPY44OWrKtcPwPhkkJ5+HxPvB+npn2LIOU3POz6crgDBHK7pFSD6YgMfBoZi41GFSCc1
-        wH8DGwH2dw9ze0sFHm8Ip8uPwxWgp9/HJacfdxr4DJqKDdiOaJEriP9nnwTc+TaqJLULECMwACy6
-        jrbjEBcRrXEpBnOp0BD5AAJ8BngZyBSVc14XH6QyXWFZgK2I/3O/Fhv0da+Loooqqqiiiirqg9H/
-        ADXHZvFxisQUAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIzLTAzLTE3VDE0OjIwOjU0KzAwOjAwVZhm
-        ewAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMy0wMy0xN1QxNDoyMDo1NCswMDowMCTF3scAAAAodEVY
-        dGRhdGU6dGltZXN0YW1wADIwMjMtMDMtMTdUMTQ6MjA6NTQrMDA6MDBz0P8YAAAAAElFTkSuQmCC"
+      <path
+        d="M37.099 149.666h61.753v79.405H37.099z"
+        style={{
+          opacity: 1,
+          fill: "#ffe14d",
+          fillOpacity: 1,
+          stroke: "none",
+          strokeWidth: 0.40771905,
+          strokeMiterlimit: 4,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        transform="matrix(.9729 -.23125 .10458 .99452 -45.305 -113.199)"
+      />
+      <path
+        fill="none"
+        d="M454.12 772.56c-.013-.694-2.224-22.027-4.914-47.407l-4.89-46.145 7.187-7.631c23.748-25.22 186.02-195.303 186.189-195.152.482.427 20.77 124.519 24.032 146.99 8.385 57.766 12.822 101.06 11.223 109.521-.584 3.094-1.386 4.139-4.015 5.238-8.673 3.623-88.862 17.484-183.808 31.77-30.093 4.53-30.97 4.609-31.004 2.817z"
+        style={{
+          opacity: 1,
+          fill: "#000",
+          fillOpacity: 0,
+          stroke: "none",
+          strokeWidth: 1.79823053,
+          strokeMiterlimit: 4,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        transform="translate(-45.305 -113.199) scale(.26458)"
+      />
+      <path
+        d="M454.12 772.56c-.013-.694-2.224-22.027-4.914-47.407l-4.89-46.145 7.187-7.631c23.748-25.22 186.02-195.303 186.189-195.152.482.427 20.77 124.519 24.032 146.99 8.385 57.766 12.822 101.06 11.223 109.521-.584 3.094-1.386 4.139-4.015 5.238-8.673 3.623-88.862 17.484-183.808 31.77-30.093 4.53-30.97 4.609-31.004 2.817z"
+        style={{
+          opacity: 1,
+          fill: "#fabe2c",
+          fillOpacity: 1,
+          stroke: "#fabe2c",
+          strokeWidth: 1.79823053,
+          strokeMiterlimit: 4,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        transform="translate(-45.305 -113.199) scale(.26458)"
+      />
+      <path
+        d="M438.921 626.725c-.039-.49-3.363-31.8-7.387-69.58-4.024-37.78-7.176-68.877-7.005-69.106.68-.91 72.522-10.672 108.407-14.73 26.22-2.967 51.518-4.369 51.518-2.856 0 4-25.185 33.827-53.666 63.555-27.017 28.2-91.798 93.58-91.867 92.717z"
+        style={{
+          opacity: 1,
+          fill: "#ff9100",
+          fillOpacity: 1,
+          stroke: "#ff9100",
+          strokeWidth: 2.93291354,
+          strokeMiterlimit: 4,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        transform="translate(-45.305 -113.199) scale(.26458)"
+      />
+      <path
+        d="M379.63 598.745c-60.34-35.06-77.915-45.662-100.848-60.834-18.71-12.379-26.164-18.562-23.902-19.827 2.544-1.424 25.273-6.581 45.62-10.351 42.343-7.846 121.508-20.335 122.516-19.327.326.326 3.884 31.831 7.905 70.01 7.268 69.007 7.443 72.103 4.058 71.896-.626-.039-25.533-14.244-55.349-31.567z"
+        style={{
+          opacity: 1,
+          fill: "#fdba12",
+          fillOpacity: 1,
+          stroke: "none",
+          strokeWidth: 1.79823053,
+          strokeMiterlimit: 4,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        transform="translate(-45.305 -113.199) scale(.26458)"
+      />
+      <path
+        d="M57.728 216.205c-2.134-3.415-6.145-22.668-8.913-42.785-4.455-32.375-4.562-37.049-.93-40.681 2.691-2.691 24.324-7.099 62.837-12.803 50.746-7.516 59.267-8.166 62.653-4.78 4.495 4.495 16.064 74.8 13.591 82.592-.885 2.788-3.718 5.718-6.296 6.51-3.73 1.148-114.342 18.155-118.076 18.155-.543 0-2.732-2.794-4.866-6.208zm67.212-11.91c28.073-4.125 51.872-8.255 52.886-9.177 1.57-1.428-1.225-24.027-7.472-60.41l-1.558-9.071-25.563 26.84-25.564 26.84-31.457-18.315c-17.302-10.073-31.754-18.018-32.116-17.656-.716.716 6.43 50.829 9.028 63.31 1.141 5.484 2.92 7.329 6.187 6.417 2.522-.703 27.555-4.654 55.629-8.779zm29.75-79.97c-1.995-1.995-87.691 10.683-87.691 12.972 0 1.044 10.864 8.184 24.142 15.867l24.142 13.968 20.403-20.704c11.221-11.388 19.773-21.334 19.004-22.104z"
+        style={{
+          fill: "#000",
+          strokeWidth: 2.75909686,
+        }}
+        transform="translate(-45.305 -113.199)"
       />
     </svg>
   );
