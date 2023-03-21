3a6467574d36a1cba31f71e2a955193ca8549239
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2023-03-21 01:52:05 +0000

    Refactor: ArrowButton.stories.tsx

--- components/ArrowButton/ArrowButton.stories.tsx
+++ components/ArrowButton/ArrowButton.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { ArrowButton } from "./ArrowButton";
 
 export default {
@@ -6,18 +6,16 @@ export default {
   component: ArrowButton,
 } as Meta<typeof ArrowButton>;
 
-const Template: StoryFn<typeof ArrowButton> = (args) => (
-  <ArrowButton {...args} />
-);
-
-export const Disabled = Template.bind({});
-Disabled.args = {
-  disabled: true,
-  handleClick: () => null,
+export const Disabled: StoryObj<typeof ArrowButton> = {
+  args: {
+    disabled: true,
+    handleClick: () => null,
+  },
 };
 
-export const ToRight = Template.bind({});
-ToRight.args = {
-  direction: "right",
-  handleClick: () => null,
+export const ToRight: StoryObj<typeof ArrowButton> = {
+  args: {
+    direction: "right",
+    handleClick: () => null,
+  },
 };
