880e1f1d98e9b1336bd4ce55fee82975dd900453
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2023-03-22 02:16:36 +0000

    Refactor ArrowButton.stories.tsx

--- components/ArrowButton/ArrowButton.stories.tsx
+++ components/ArrowButton/ArrowButton.stories.tsx
@@ -1,21 +1,29 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
 import { ArrowButton } from "./ArrowButton";
 
 export default {
   title: "Arrow button",
   component: ArrowButton,
+  argTypes: {
+    onClick: { action: "Clicked" },
+  },
 } as Meta<typeof ArrowButton>;
 
 export const Disabled: StoryObj<typeof ArrowButton> = {
   args: {
     disabled: true,
-    handleClick: () => null,
   },
 };
 
 export const ToRight: StoryObj<typeof ArrowButton> = {
   args: {
     direction: "right",
-    handleClick: () => null,
   },
+};
+
+ToRight.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.click(canvas.getByRole("button"));
 };
