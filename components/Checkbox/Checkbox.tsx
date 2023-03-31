a812213c497b60a2ed10d915b319401c429b132f
Dialejo24 <dialejosi530@gmail.com>
2023-03-31 15:58:20 -0500

    Cambia el estilo del cursor al hacer hover en componente link

--- components/Checkbox/Checkbox.tsx
+++ components/Checkbox/Checkbox.tsx
@@ -11,7 +11,7 @@ export const Checkbox = ({ label, id, ...props }: CheckboxProps) => {
     "peer z-10 appearance-none w-5 min-w-[1.25rem] h-5 border rounded-[2px] border-blue cursor-pointer checked:border-orange checked:bg-orange hover:enabled:border-2 focus:outline-none disabled:cursor-not-allowed disabled:border-gray-dark disabled:bg-gray-medium";
 
   const labelStyle =
-    "z-10 text-blue text-sm font-outfit ml-2 peer-checked:text-orange font-medium peer-disabled:text-gray-dark peer-disabled:font-medium";
+    "z-10 text-blue text-sm font-outfit ml-2 peer-checked:text-orange font-medium hover:cursor-pointer peer-disabled:text-gray-dark peer-disabled:font-medium";
 
   const formattedId = id.toLowerCase().split(" ").join("");
 
