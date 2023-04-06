9f029cb44e9eeca5b662d5cc1b7f2117d14bba52
Gastón Echegoyen <gastonec24@gmail.com>
2023-04-06 18:40:24 -0300

    wip: add external structure and slides styles

---
+++ components/Carousel/Carousel.stories.tsx
@@ -0,0 +1,49 @@
+import { Meta } from "@storybook/react";
+import Carousel, { CarouselSlideData } from "./Carousel";
+
+export default {
+  title: "Carousel",
+  component: Carousel,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+} as Meta<typeof Carousel>;
+
+const mockSlides: CarouselSlideData[] = [
+  {
+    title: "Dragón",
+    description: "Dragón que emite luces y sonido cuando se pulsa el botón.",
+    isAvailable: true,
+    link: "/",
+  },
+  {
+    title: "Dragón",
+    description: "Dragón que emite luces y sonido cuando se pulsa el botón.",
+    isAvailable: true,
+    link: "/",
+  },
+  {
+    title: "Dragón",
+    description: "Dragón que emite luces y sonido cuando se pulsa el botón.",
+    isAvailable: true,
+    link: "/",
+  },
+  {
+    title: "Dragón",
+    description: "Dragón que emite luces y sonido cuando se pulsa el botón.",
+    isAvailable: true,
+    link: "/",
+  },
+  {
+    title: "Dragón",
+    description: "Dragón que emite luces y sonido cuando se pulsa el botón.",
+    isAvailable: true,
+    link: "/",
+  },
+];
+
+export const SimpleCarousel = () => {
+  return <Carousel slides={mockSlides} />;
+};
---
+++ components/Carousel/Carousel.tsx
@@ -0,0 +1,123 @@
+"use client";
+import Link from "next/link";
+import styled from "styled-components";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Button } from "components/Button/Button";
+import { IconCheckmark } from "components/IconCheckmark/IconCheckmark";
+
+export interface CarouselSlideData {
+  title: string;
+  img?: string;
+  isAvailable?: boolean;
+  description?: string;
+  btnText?: string;
+  link?: string;
+}
+
+interface CarouselData {
+  slides: CarouselSlideData[];
+}
+
+const StyledSwiper = styled(Swiper)`
+  height: 467px;
+  max-width: 1020px;
+
+  .swiper-wrapper {
+    max-width: 858px;
+
+    .swiper-slide {
+      width: 267px;
+    }
+  }
+`;
+
+const StyledSlide = styled.div`
+  width: 273px;
+  height: 467px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
+  text-align: left;
+  gap: 16px;
+
+  border-radius: 4px;
+  padding: 24px;
+
+  background-color: white;
+`;
+
+const StyledLink = styled(Link)`
+  width: 100%;
+`;
+
+const ImageDiv = styled.div`
+  width: 226px;
+  height: 168px;
+
+  border-radius: 4px;
+
+  background-color: gray;
+`;
+
+const CarouselSlide = ({
+  title,
+  img,
+  isAvailable,
+  description,
+  btnText = "PEDIR JUGUETE",
+  link = "/",
+}: CarouselSlideData) => {
+  return (
+    <StyledSlide>
+      <div className="flex w-full flex-initial border-b border-solid border-gray py-3">
+        <h4 className="font-outfit text-xl">{title}</h4>
+      </div>
+
+      <ImageDiv />
+
+      <div className="font-outfit text-sm">
+        {isAvailable ? (
+          <div className="flex flex-row items-center font-outfit font-bold text-green">
+            Disponible &nbsp; <IconCheckmark className="fill-green" />
+          </div>
+        ) : (
+          <div></div>
+        )}{" "}
+      </div>
+
+      <p className="font-outfit text-sm font-medium text-blue">{description}</p>
+
+      <StyledLink href={link}>
+        <Button appearance="button">{btnText}</Button>
+      </StyledLink>
+    </StyledSlide>
+  );
+};
+
+const Carousel = ({ slides }: CarouselData) => {
+  return (
+    <StyledSwiper
+      modules={[Navigation]}
+      direction="horizontal"
+      slidesPerView={3}
+      spaceBetween={19}
+      navigation
+    >
+      {slides.map((slide) => {
+        return (
+          <SwiperSlide key={slide.title}>
+            <CarouselSlide {...slide} />
+          </SwiperSlide>
+        );
+      })}
+    </StyledSwiper>
+  );
+};
+
+export default Carousel;
