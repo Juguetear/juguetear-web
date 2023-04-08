1ee262845df472d23c9e0d8db2899280daba7a73
Anthony Mackensen <tonymckes@gmail.com>
2023-04-08 12:43:57 -0400

    feat: add version

--- lib/sanity-variables.ts
+++ lib/sanity-variables.ts
@@ -2,6 +2,6 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "isi1r0nm";
 const title = process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || "Juguetear";
 const token = process.env.NEXT_PUBLIC_SANITY_API_READ_TOKEN;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "v1";
 
 export { dataset, projectId, title, token, apiVersion };
