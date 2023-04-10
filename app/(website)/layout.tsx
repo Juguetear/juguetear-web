0fa58e0039f4197229094a1c1e0c83289dc66a23
Anthony Mackensen <tonymckes@gmail.com>
2023-04-10 18:27:17 -0400

    feat: add fetch and layout schema types

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,13 +1,23 @@
+import { client } from "lib/client";
+import { groq } from "next-sanity";
+import type { LayoutSchema } from "types/layout-schema-types";
+
 interface Props {
   children: React.ReactNode;
 }
 
 export default async function WebsiteLayout({ children }: Props) {
+  const { navbarLinks, footerLinks } = await client.fetch<LayoutSchema>(
+    groq`*[_type == 'layout'][0]`
+  );
+
   return (
     <>
       {/* TODO: Agregar componente Navbar */}
+      {/* <pre>{JSON.stringify(navbarLinks, null, 2)}</pre> */}
       <main>{children}</main>
       {/* TODO: Agregar componente Footer */}
+      {/* <pre>{JSON.stringify(footerLinks, null, 2)}</pre> */}
     </>
   );
 }
