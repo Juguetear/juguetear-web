a3221d9c173b4abec19355b7ab89dfad0b54c02d
alexxuss1996 <alekseymelnych132@gmail.com>
2023-04-17 17:06:18 +0300

    Make some additional fixes

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -14,7 +14,7 @@ export interface HeroProps extends React.HTMLProps<HTMLElement> {
 export const Hero = ({ title, subTitle, image }: HeroProps) => {
   return (
     <section className="relative flex max-w-screen-xl flex-col overflow-hidden py-10 lg:ml-auto lg:flex-row lg:items-center">
-      <div className="mb-14 mr-2 lg:w-6/12">
+      <div className="mb-14 lg:mb-0 lg:w-6/12">
         <h1 className="mb-1 max-w-4xl text-blue md:mb-5">{title}</h1>
         <p className="mb-8 font-inter text-base font-medium text-blue md:mb-9 md:text-xl">
           {subTitle}
@@ -25,7 +25,7 @@ export const Hero = ({ title, subTitle, image }: HeroProps) => {
         src={image.src}
         width="750"
         height="478"
-        className="relative w-full lg:left-36 lg:w-8/12 lg:rounded-[56px] xl:left-32 xl:w-full"
+        className="relative w-full object-center lg:left-36 lg:w-8/12 lg:rounded-[56px] xl:left-32 xl:w-full"
         alt={image.alt}
       />
     </section>
