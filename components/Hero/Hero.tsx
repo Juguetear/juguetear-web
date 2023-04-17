5d9f5a130b6b8fe40fc8f8391a61a06b1f09133f
alexxuss1996 <alekseymelnych132@gmail.com>
2023-04-17 16:19:24 +0300

    Make visual changes to Hero component

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -13,23 +13,21 @@ export interface HeroProps extends React.HTMLProps<HTMLElement> {
 
 export const Hero = ({ title, subTitle, image }: HeroProps) => {
   return (
-    <section className="relative py-10">
-      <div className="flex max-w-screen-xl flex-col lg:ml-auto lg:flex-row lg:items-center">
-        <div className="mb-14 mr-2 lg:w-6/12">
-          <h1 className="max-w-4xl text-blue">{title}</h1>
-          <p className="my-4 mb-9 font-inter text-xl font-medium text-blue">
-            {subTitle}
-          </p>
-          <Button appearance="button">Ver juguetes</Button>
-        </div>
-        <Image
-          src={image.src}
-          width="750"
-          height="478"
-          className="relative w-full lg:left-36 lg:w-8/12 lg:rounded-[56px] xl:left-32 xl:w-full"
-          alt={image.alt}
-        />
+    <section className="relative flex max-w-screen-xl flex-col overflow-hidden py-10 lg:ml-auto lg:flex-row lg:items-center">
+      <div className="mb-14 mr-2 lg:w-6/12">
+        <h1 className="mb-1 max-w-4xl text-blue md:mb-5">{title}</h1>
+        <p className="mb-8 font-inter text-base font-medium text-blue md:mb-9 md:text-xl">
+          {subTitle}
+        </p>
+        <Button appearance="button">Ver juguetes</Button>
       </div>
+      <Image
+        src={image.src}
+        width="750"
+        height="478"
+        className="relative w-full lg:left-36 lg:w-8/12 lg:rounded-[56px] xl:left-32 xl:w-full"
+        alt={image.alt}
+      />
     </section>
   );
 };
