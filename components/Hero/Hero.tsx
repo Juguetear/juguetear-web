cb984054081b801288462e3a746e568d627a07f5
alexxuss1996 <alekseymelnych132@gmail.com>
2023-04-19 23:02:02 +0300

    Fix and add wrapper for image

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -14,7 +14,7 @@ export interface HeroSection extends React.HTMLProps<HTMLElement> {
 export const Hero = ({ title, description, photo, cta }: HeroSection) => {
   return (
     <section className="relative py-10">
-      <div className="flex max-w-screen-xl flex-col overflow-hidden lg:mx-auto lg:flex-row lg:items-center">
+      <div className="flex max-w-screen-xl flex-col lg:mx-auto lg:flex-row lg:items-center">
         <div className="mb-14 lg:mb-0 lg:w-5/12">
           <h1 className="mb-1 max-w-4xl text-blue md:mb-5">{title}</h1>
           <p className="mb-8 font-inter text-base font-medium text-blue md:mb-9 md:text-xl">
@@ -24,13 +24,15 @@ export const Hero = ({ title, description, photo, cta }: HeroSection) => {
             {cta.name}
           </Link>
         </div>
-        <Image
-          src={urlFor(photo).url()}
-          width={750}
-          height={478}
-          className="relative w-full object-center lg:left-36 lg:w-8/12 lg:rounded-l-[56px] xl:left-40 xl:w-full"
-          alt={photo.altText}
-        />
+        <span className="overflow-hidden">
+          <Image
+            src={urlFor(photo).url()}
+            className="lg:translate-x-20 lg:rounded-l-[56px]"
+            width={750}
+            height={478}
+            alt={photo.altText}
+          />
+        </span>
       </div>
     </section>
   );
