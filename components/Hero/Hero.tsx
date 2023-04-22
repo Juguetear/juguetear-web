41f1e815785d757f58048df196d023b110b19518
alexxuss1996 <alekseymelnych132@gmail.com>
2023-04-22 16:49:51 +0300

    Revert 76d8e78
    This reverts commit 76d8e7898043d18f9eebf1248bcb7cb019dc010f.

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -15,7 +15,7 @@ export const Hero = ({ title, description, photo, cta }: HeroSection) => {
   return (
     <section className="py-10">
       <div className="flex max-w-screen-xl flex-col lg:mx-auto lg:flex-row lg:items-center">
-        <article className="mb-14 lg:mb-0 lg:w-5/12">
+        <div className="mb-14 lg:mb-0 lg:w-5/12">
           <h1 className="mb-1 max-w-4xl text-blue md:mb-5">{title}</h1>
           <p className="mb-8 font-inter text-base font-medium text-blue md:mb-9 md:text-xl">
             {description}
@@ -23,8 +23,8 @@ export const Hero = ({ title, description, photo, cta }: HeroSection) => {
           <Link href={cta.url} appearance="button">
             {cta.name}
           </Link>
-        </article>
-        <figure className="overflow-hidden">
+        </div>
+        <span className="overflow-hidden">
           <Image
             src={urlFor(photo).url()}
             className="lg:translate-x-20 lg:rounded-l-[56px]"
@@ -32,7 +32,7 @@ export const Hero = ({ title, description, photo, cta }: HeroSection) => {
             height={478}
             alt={photo.altText}
           />
-        </figure>
+        </span>
       </div>
     </section>
   );
