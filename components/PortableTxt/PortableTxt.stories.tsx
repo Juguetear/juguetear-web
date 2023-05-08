a11ca4216ef676bb57bf2eea229cf3585c3d2588
LPRegen <manuel.escribano.051@gmail.com>
2023-05-08 13:17:02 +0200

    refactor: import mocked data

--- components/PortableTxt/PortableTxt.stories.tsx
+++ components/PortableTxt/PortableTxt.stories.tsx
@@ -1,76 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { PortableTxt } from "./PortableTxt";
-
-const mockedData = [
-  {
-    _type: "blockscontent",
-    style: "h1",
-    markDefs: [],
-    children: [
-      {
-        _type: "span",
-        text: "Heading 1",
-        marks: [],
-      },
-    ],
-  },
-  {
-    _type: "blockscontent",
-    style: "normal",
-    markDefs: [],
-    children: [
-      {
-        _type: "span",
-        text: "Strong text",
-        marks: ["strong"],
-      },
-    ],
-  },
-  {
-    _type: "blockscontent",
-    style: "normal",
-    markDefs: [],
-    children: [
-      {
-        _type: "span",
-        marks: ["em"],
-        text: "Emphasise text",
-      },
-    ],
-  },
-  {
-    _type: "blockscontent",
-    style: "normal",
-    markDefs: [],
-    children: [
-      {
-        _type: "span",
-        marks: ["underline"],
-        text: "Underline text",
-      },
-    ],
-  },
-  {
-    _type: "blockscontent",
-    style: "normal",
-    markDefs: [],
-    children: [
-      {
-        _type: "span",
-        marks: [],
-        text: "",
-      },
-    ],
-  },
-  {
-    _type: "blocksimage",
-    asset: {
-      _type: "reference",
-      _ref: "image-7e560aeb0267f502f5aa17b8e8ae08e3e557f81d-862x485-jpg",
-    },
-    altText: "Persona modificando dinosaurio verde de juguete",
-  },
-];
+import mockedData from "./mockedData.json";
 
 type Story = StoryObj<typeof PortableTxt>;
 
@@ -80,7 +10,7 @@ const meta: Meta<typeof PortableTxt> = {
 };
 
 export const Content: Story = {
-  render: () => <PortableTxt content={mockedData} />,
+  render: () => <PortableTxt content={mockedData.description} />,
 };
 
 export default meta;
