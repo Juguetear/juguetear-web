6a8b09731b12955508103afb76dcf0bde2a3e397
LPRegen <manuel.escribano.051@gmail.com>
2023-05-11 22:27:04 +0200

    style: remove TODO comment

--- schemas/shared/content.ts
+++ schemas/shared/content.ts
@@ -6,7 +6,6 @@ const content = defineType({
   type: "array",
   of: [
     defineArrayMember({ type: "block", title: "Texto", name: "blockscontent" }),
-    // TODO: remove img schema
     defineArrayMember({ type: "images" }),
   ],
 });
