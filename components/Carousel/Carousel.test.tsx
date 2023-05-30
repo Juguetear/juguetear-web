053ec3db339706fd6fe0fba6618a85b92e4c1fe0
Damian Palavecino <damianpalavecino25@gmail.com>
2023-05-30 03:09:29 -0300

    test: add carousel content variable for readability

--- components/Carousel/Carousel.test.tsx
+++ components/Carousel/Carousel.test.tsx
@@ -11,27 +11,22 @@ import Carousel from "./Carousel";
 jest.mock("swiper/css/navigation", jest.fn());
 
 describe("Carousel", () => {
+  const carouselContent = (
+    <>
+      <div>Slide 1</div>
+      <div>Slide 2</div>
+      <div>Slide 3</div>
+    </>
+  );
   it("renders the Carousel component with children", () => {
-    render(
-      <Carousel>
-        <div>Slide 1</div>
-        <div>Slide 2</div>
-        <div>Slide 3</div>
-      </Carousel>
-    );
+    render(<Carousel>{carouselContent}</Carousel>);
     expect(screen.getByText("Slide 1")).toBeInTheDocument();
     expect(screen.getByText("Slide 2")).toBeInTheDocument();
     expect(screen.getByText("Slide 3")).toBeInTheDocument();
   });
 
   it("renders the Carousel component with two ArrowButton components", () => {
-    render(
-      <Carousel>
-        <div>Slide 1</div>
-        <div>Slide 2</div>
-        <div>Slide 3</div>
-      </Carousel>
-    );
+    render(<Carousel>{carouselContent}</Carousel>);
     const leftArrowButton = screen.getByLabelText(/anterior/i);
     const rightArrowButton = screen.getByLabelText(/siguiente/i);
     expect(leftArrowButton).toBeInTheDocument();
