9ca632fa55cc169c89176896a56b923593f63eaa
Anthony Mackensen <tonymckes@gmail.com>
2023-06-02 11:44:14 -0400

    refactor: sanity studio page

--- app/(sanity)/studio/[[...index]]/page.tsx
+++ app/(sanity)/studio/[[...index]]/page.tsx
@@ -1,17 +1,11 @@
-"use client";
+import type { Metadata } from "next";
+import { metadata as studioMetadata } from "next-sanity/studio/metadata";
+import { Studio } from "./Studio";
 
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio v3.
- * All routes under /studio will be handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
-
-import { NextStudio } from "next-sanity/studio";
-import config from "sanity.config";
+export const metadata: Metadata = {
+  ...studioMetadata,
+};
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <Studio />;
 }
