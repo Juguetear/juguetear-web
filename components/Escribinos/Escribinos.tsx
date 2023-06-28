8424efbb146bb8a3d27ecf08ae0722c7d9ba06ca
alexxuss1996 <alekseymelnych132@gmail.com>
2023-06-28 21:43:57 +0300

    [Fix]: Errors and Adjustments

--- components/Escribinos/Escribinos.tsx
+++ components/Escribinos/Escribinos.tsx
@@ -6,36 +6,37 @@ import React from "react";
 export const Escribinos = () => {
   return (
     <section className="p-10">
-      <h2 className="mb-10 text-center text-3xl font-bold text-blue">
-        Escribinos
-      </h2>
-      <p className="mb-10 text-center">
-        Los campos que contengan * son obligatorios
-      </p>
-      <form action="" method="POST">
-        <Input
-          helperText="Nombre"
-          id="name"
-          name="name"
-          label="Nombre completo:"
-          type="text"
-          required
-        />
-        <Input
-          helperText="Email"
-          id="enail"
-          name="email"
-          label="Email:"
-          type="email"
-          required
-        />
-        <TextArea helperText="Mensaje:" label="Mensaje:" required />
-        <div className="flex justify-center md:justify-end">
-          <Button appearance="button" type="submit">
-            Enviar
-          </Button>
-        </div>
-      </form>
+      <div className="mx-auto max-w-3xl">
+        <h2 className="mb-8 text-center">Escribinos</h2>
+        <p className="mb-8 text-center">
+          Los campos que contengan * son obligatorios
+        </p>
+
+        <form action="" className="mx-auto max-w-2xl" method="POST">
+          <Input
+            helperText="Nombre"
+            id="name"
+            name="name"
+            label="Nombre completo:"
+            type="text"
+            required
+          />
+          <Input
+            helperText="Email"
+            id="email"
+            name="email"
+            label="Email:"
+            type="email"
+            required
+          />
+          <TextArea helperText="Mensaje:" label="Mensaje:" required />
+          <div className="flex justify-center md:justify-end">
+            <Button appearance="button" type="submit">
+              Enviar
+            </Button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
