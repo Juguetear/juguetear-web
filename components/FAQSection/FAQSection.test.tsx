d0e667bc30f6dfd00d3b59305c95547b947c2a57
alexxuss1996 <alekseymelnych132@gmail.com>
2023-09-09 13:42:28 +0300

    Add tests

--- components/FAQSection/FAQSection.test.tsx
+++ components/FAQSection/FAQSection.test.tsx
@@ -5,5 +5,8 @@ import { FAQMockData } from "components/FAQSection/FAQMockData";
 describe("FAQSection", () => {
   it("should render", () => {
     render(<FAQSection {...FAQMockData} />);
+
+    expect(screen.getByRole("heading", { level: 2 })).toBeInTheDocument();
+    expect(screen.getByText(FAQMockData.paragraph)).toBeInTheDocument();
   });
 });
