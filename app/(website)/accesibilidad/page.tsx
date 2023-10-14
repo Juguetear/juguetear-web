d93a52d21190046ea2d12f32efd66690fd4b4312
alexxuss1996 <alekseymelnych132@gmail.com>
2023-10-14 19:57:55 +0300

    [Feat]: Create groq query for accessibility Page

---
+++ app/(website)/accesibilidad/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { client } from "lib/sanity-client";
+import { accessibilityDeclarationPageQuery } from "lib/sanity-queries";
+import { AccessibilityDeclarationPage } from "types/accessibility-declaration-page";
+
+async function Page() {
+  const accessibilidadPage = await client.fetch<AccessibilityDeclarationPage>(
+    accessibilityDeclarationPageQuery
+  );
+
+  /* TODO Crear Accessibilidad Page */
+  return <>{/* <pre>{JSON.stringify(accessibilidadPage, null, 2)}</pre> */}</>;
+}
+
+export default Page;
