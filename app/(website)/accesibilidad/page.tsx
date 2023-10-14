31edbf3522d1566c02696d95ff21f676cb318659
alexxuss1996 <alekseymelnych132@gmail.com>
2023-10-14 20:08:27 +0300

    refactor: imports declaration

--- app/(website)/accesibilidad/page.tsx
+++ app/(website)/accesibilidad/page.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import { client } from "lib/sanity-client";
 import { accessibilityDeclarationPageQuery } from "lib/sanity-queries";
-import React from "react";
 import { AccessibilityDeclarationPage } from "types/accessibility-declaration-page";
 
 async function Page() {
