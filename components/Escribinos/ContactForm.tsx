fbb643200dfb85548375d1f6daafaf196a51ed9c
LPRegen <manuel.escribano.051@gmail.com>
2023-10-20 17:57:31 +0200

    refactor: display notification after submission

--- components/Escribinos/ContactForm.tsx
+++ components/Escribinos/ContactForm.tsx
@@ -1,29 +1,67 @@
 "use client";
 
+import Notification from "components/Notification/Notification";
+import EnvelopeIcon from "components/assets/EnvelopeIcon";
 import { postClient } from "lib/sanity-client";
+import { useState } from "react";
 
 interface ContactFormProps {
   children: React.ReactNode;
 }
 
 export const ContactForm = ({ children }: ContactFormProps) => {
+  const [displayNotif, setDisplayNotif] = useState<boolean>(false);
+  const [notifMsg, setNotifMsg] = useState<string>("");
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
+    // TODO: Add validation
     const { name, email, body } = Object.fromEntries(formData);
 
-    const response = await postClient.create({
-      _type: "message",
-      name,
-      email,
-      body,
-    });
+    const response = await postClient
+      .create({
+        _type: "_message",
+        name,
+        email,
+        body,
+      })
+      .then(() =>
+        setNotifMsg("¡Tu mensaje fue enviado! Te responderemos a la brevedad.")
+      )
+      .catch(() =>
+        setNotifMsg(
+          "Hubo un error al intentar enviar tu mensaje, intentalo nuevamente."
+        )
+      )
+      .finally(() => setDisplayNotif((display) => !display));
     return response;
   };
 
+  const handleClick = () => {
+    setDisplayNotif((display) => !display);
+  };
+
   return (
-    <form className="container max-w-2xl" onSubmit={handleSubmit}>
-      {children}
-    </form>
+    <>
+      {displayNotif ? (
+        <Notification>
+          <div className="flex justify-center">
+            {/* TODO: Display another icon than EnvelopeIcon in case of an error. */}
+            <EnvelopeIcon className="shrink-0" />
+            <div className="ml-5 text-center">
+              <p>{notifMsg}</p>
+              <button className="font-semibold" onClick={handleClick}>
+                Enviar otro mensaje
+              </button>
+            </div>
+          </div>
+        </Notification>
+      ) : (
+        <form className="container max-w-2xl" onSubmit={handleSubmit}>
+          {children}
+        </form>
+      )}
+    </>
   );
 };
