d7df52cc32e3eb746f5fa725caa69adf8f2c22f7
beetlejuice96 <dilanbernbae043@gmail.com>
2024-03-15 21:13:11 -0300

    add portableText component to colaborar page

--- app/(website)/colaborar/page.tsx
+++ app/(website)/colaborar/page.tsx
@@ -1,13 +1,13 @@
+import { PortableTxt } from "components/PortableTxt/PortableTxt";
 import { client } from "lib/sanity-client";
 import { collaboratePageQuery } from "lib/sanity-queries";
 import { CollaboratePage } from "types/collaborate-page";
 
 async function Page() {
-  const colaborarData = await client.fetch<CollaboratePage>(
-    collaboratePageQuery
-  );
+  const colaborarData =
+    await client.fetch<CollaboratePage>(collaboratePageQuery);
   /* TODO: Crear colaborar page */
-  return <>{JSON.stringify(colaborarData, null, 2)}</>;
+  return <PortableTxt content={colaborarData.description}></PortableTxt>;
 }
 
 export default Page;
